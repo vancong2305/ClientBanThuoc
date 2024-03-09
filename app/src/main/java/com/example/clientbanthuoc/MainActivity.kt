2ac558026808f75c941918a086ef80119d3f0872
Pham Dang Khoa <156926024+phamdangkhoagh@users.noreply.github.com>
2024-03-09 13:35:41 +0700

    Screen

--- app/src/main/java/com/example/clientbanthuoc/MainActivity.kt
+++ app/src/main/java/com/example/clientbanthuoc/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.clientbanthuoc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.graphics.Paint;
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.profile_screen)
+        setContentView(R.layout.product_screen)
+
+
     }
 }
